62c4f59b49f90da6538f69f2df37d35999fb6af0
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-07 11:52:49 +0200

    second task

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML ir <?php print 'PHP!';?></title>
 </head>
 <body>
-    <h1><?php print 'Juozukas'; ?> - HTML asas!</h1>
-    <p>Moku kurti statinį turinį.</p>
+    <h1>Juozukas - HTML <?php print 'ir PHP';?> asas!</h1>
+    <p>Kuriu dinaminį turinį. Atidaryta: <?php print $today = date("Y-m-d H:i:s"); ?></p>
 </body>
 </html>
