aff5564cd87c957f44f80e2bb0f444c844e57107
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-07 12:15:03 +0200

    3 task

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<?php date_default_timezone_set('Europe/Vilnius');?>
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+date_default_timezone_set('Europe/Vilnius');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aš valdau HTML ir <?php print 'PHP!';?></title>
+    <title> <?php print 'Aš, ' . date("l") . ' ir PHP' ?> </title>
 </head>
 <body>
-    <h1>Juozukas - HTML <?php print 'ir PHP';?> asas!</h1>
-    <p>Kuriu dinaminį turinį. Atidaryta: <?php print $today = date("Y-m-d H:i:s"); ?></p>
+    <h1>Juozukas - HTML <?php print 'ir PHP';?> asas jau nuo <?php print date("Y") . ' metų' ?> </h1>
+    <p>Viskas prasidėjo <?php print date("F") . ' mėnesio, ' . date("j") . ' dieną!'?></p>
+    
 </body>
 </html>
