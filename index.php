98bdd9b23db031d58813232c114ecdb9f21c1f44
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-07 15:23:55 +0200

    9task dice

--- index.php
+++ index.php
@@ -12,14 +12,17 @@ date_default_timezone_set('Europe/Vilnius');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> PHP lydės <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'year')) . '!'; ?></title>
 </head>
-<body style='background-color: rgb(<?php print rand(0, 255) . ',' .rand(0, 255) . ',' .rand(0, 255) ?> )'>
+<body>
 
 
-    <h1 style='font-size: <?php print rand(0, 30);?>px;'>As keiciu savo dydi</h1>
+    <!-- <h1 style='font-size: <?php print rand(0, 30);?>px;'>As keiciu savo dydi</h1>
+    <p style='color: rgb(<?php print rand(0, 255) . ',' .rand(0, 255) . ',' .rand(0, 255); ?>)'>As keiciu spalvas</p> -->
 
-    <p style='color: rgb(<?php print rand(0, 255) . ',' .rand(0, 255) . ',' .rand(0, 255) ?> )'>As keiciu spalvas</p>
 
+    
+    <img style='width: 200px; height: 200px' src="images/<?php print rand(1, 6)?>.png" alt="">
 
+    
 </body>
 </html>
 
