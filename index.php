76bfb2ad996111e4baccb9f3d7e2d6662a3c5f80
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-07 11:34:28 +0200

    final commit

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>As valdau HTML!</title>
+    <title>Aš valdau HTML!</title>
 </head>
 <body>
-    <h1><?php print '<b>Juozukas</b> - HTML asas!';?></h1>
-    <p>Moku kurti statini turini</p>
+    <h1><?php print 'Juozukas'; ?> - HTML asas!</h1>
+    <p>Moku kurti statinį turinį.</p>
 </body>
 </html>
