d3c382ac8b70ce3f6a6b72c003a237bf74623f36
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-07 14:32:35 +0200

    7 task

--- index.php
+++ index.php
@@ -10,13 +10,15 @@ date_default_timezone_set('Europe/Vilnius');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> PHP lydės ir <?php print date('Y-m-d', strtotime('+1 day')) . '!' ?></title>
+    <title> PHP lydės <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'year')) . '!'; ?></title>
 </head>
 <body>
-    <h1>Vardas - PHP su manim buvo ir <?php print date('H', strtotime('-1 hour')) . ' valandą!' ?>   </h1>
+    <h1>Vardas - Galbūt turėsiu <?php print rand(1, 5) . ' vaikų(us)!' ?></h1>
 
-    
-    <p><?php print date('Y', strtotime('+1 year')) . ' ne už kalnų!'; ?></p>
+    <p>D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?> </p>
+
+
+
 </body>
 </html>
 
