b6b3dbc21be14357e3f9126e6a875ff92292b526
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-07 11:23:25 +0200

    second commit

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-print 'veikia';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>As valdau HTML!</title>
+</head>
+<body>
+    <h1><?php print '<b>Juozukas</b> - HTML asas!';?></h1>
+    <p>Moku kurti statini turini</p>
+</body>
+</html>
